0de10dccba9e1e98dc82a08e67ff74a5f5e31b9b
Renato Dantas <rexdantas@gmail.com>
2022-07-05 20:44:04 -0300

    Upgrade de versões

--- src/services/firebase.service.ts
+++ src/services/firebase.service.ts
@@ -1,5 +1,5 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -17,3 +17,4 @@ const firebaseConfig = {
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
+export const database = getDatabase(app);
